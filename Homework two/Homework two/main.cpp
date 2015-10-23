74841e7e9ba3eb7f0b5b5078d58af754b656e2dc
Anton94 <antonvdudov@gmail.com>
2015-10-23 20:06:43 +0300

    Some optimizations for the HPC homework two. NOTE- finding the Most significant bit is not realy optimization- basic test shows worst results

--- Homework two/Homework two/main.cpp
+++ Homework two/Homework two/main.cpp
@@ -5,8 +5,12 @@
 *	antonvdudov@gmail.com
 *
 *	Github repository: https://github.com/Anton94/High-performance-computing/tree/master/Homework%20two
+*
+*
+*
+*
 *   TO DO : Delete time checker!!!!
-*	TO DO : make a test where I don`t reverse all bits, only from the MSB to the end.
+*	TO DO : Make a test where I don`t reverse all bits, only from the MSB to the end.
 */
 
 #include <stdio.h>
@@ -15,29 +19,29 @@ using namespace std::chrono;// TO DO delete it
 
 inline float derCorput(unsigned n)
 {
-	unsigned bitCount = sizeof(n) * 8;
-	bitCount -= 2; // -1 for the sign bit and -1 for the max number, I will divide by 1000...0, so 1 bit for the divider
+	unsigned bitCount = sizeof(n) * 8 ;
+	bitCount -= 2; // -1 for the sign bit (for the float dividing) and -1 for the max number, I will divide by 1000...0, so 1 bit for the divider
+
 	unsigned reversedBinary = 0;
 	for (unsigned i = 0; i < bitCount; ++i)
 	{
-		reversedBinary <<= 1;
-		if ((n >> i) & 1)
-			++reversedBinary;
+		reversedBinary <<= 1;		
+		reversedBinary |= ((n >> i) & 1); // If ((n >> i) & 1 == 1) THEN reversedBinary |= 1  ; (reversedBinary |= 1 === ++reversedBinary)
 	}
 
-	return (float)reversedBinary / (float)(1 << 30); // TO DO- optimize
+	return (float)reversedBinary / (float)(1 << 30);
 }
 
 // Test - calculate the numbers from 0 to the given @n with/without printing it. 
 // If @flag- than print the values.
 void test1(unsigned n, char flag)
 {
-	printf("Test: get the numbers in sequence from 0 to %d\n", n);
+	printf("Test: get the numbers in the van der Corput sequence from 0 to %d\n", n);
 	auto begin = high_resolution_clock::now();
 
 	if (flag)
 		for (unsigned i = 0; i < n; ++i)
-			printf("%d number in the van der Corput sequence is %f\n", i, derCorput(i));
+			printf("%unumber in the van der Corput sequence is %f\n", i, derCorput(i));
 	else
 		for (unsigned i = 0; i < n; ++i)
 			derCorput(i);
@@ -52,5 +56,30 @@ int main()
 {
 	test1(20, 1);
 	test1(20000000, 0);
+
 	return 0;
 }
+
+
+
+
+
+
+/*
+	For tests...
+
+	inline float derCorputWithFindingTheMSB(unsigned n)
+	{
+		unsigned bitCount = 1 + log2(n);
+
+		unsigned reversedBinary = 0;
+		for (unsigned i = 0; i < bitCount; ++i)
+		{
+			reversedBinary <<= 1;
+			if ((n >> i) & 1)
+				++reversedBinary;
+		}
+
+		return (float)reversedBinary / (float)(1 << bitCount);
+	}
+*/
